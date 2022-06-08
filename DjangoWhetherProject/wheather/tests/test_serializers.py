2205fed5afe87defb95761663570880e3fe17fe2
roma <roma.marhatok@yandex.by>
2022-06-08 16:26:25 +0300

    change: tests for serializers

--- DjangoWhetherProject/wheather/tests/test_serializers.py
+++ DjangoWhetherProject/wheather/tests/test_serializers.py
@@ -5,11 +5,11 @@ from wheather.serializers import (
     DayCityWheatherSerializer,
     HourDayWheatherSerializer,
 )
+from django.template.defaultfilters import slugify
 
 
 @pytest.mark.django_db
-def test_city_serialization(city_factory, day_factory):
-
+def test_city_serialization(city_factory):
     city = city_factory.build()
     serializer_data = {"name": city.name, "slug": city.slug}
     serializer = CitySerializer(data=serializer_data)
@@ -17,7 +17,19 @@ def test_city_serialization(city_factory, day_factory):
     serializer.save()
     cities_count = City.objects.all().count()
 
+    assert len(serializer.errors) == 0
     assert cities_count == 1
+
+
+@pytest.mark.django_db
+def test_city_init(city_factory):
+    city = city_factory.build()
+    serializer_data = {"name": city.name}
+    serializer = CitySerializer(data=serializer_data)
+    serializer.is_valid()
+    assert len(serializer.errors) == 0
+    assert "slug" in serializer.data
+    assert serializer.data["slug"] == slugify(city.name)
 
 
 @pytest.mark.django_db
@@ -36,10 +48,12 @@ def test_hour_serialization(hour_fixture):
 
 
 @pytest.mark.django_db
-def test_get_hours(hour_fixture):
-    serializer = HourDayWheatherSerializer(instance=hour_fixture)
-    times = ["2022-06-03 00:00", "2022-06-03 23:00", "", "09:00"]
+def test_get_hours():
+    serializer = HourDayWheatherSerializer()
+    times = ["2022-06-03 00:00", "2022-06-03 23:00", "09:00", "2022-06-03", "", None]
     assert serializer.get_hours(times[0]) == 0
     assert serializer.get_hours(times[1]) == 23
-    assert serializer.get_hours(times[2]) is None
-    assert serializer.get_hours(times[3]) == 9
+    assert serializer.get_hours(times[2]) == 9
+    assert serializer.get_hours(times[3]) is None
+    assert serializer.get_hours(times[4]) is None
+    assert serializer.get_hours(times[5]) is None
